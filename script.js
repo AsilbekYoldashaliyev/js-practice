dba5a89b8835b0cf7dbbeac243d92dc7218dcc86
Asilbek217 <yoldashaliyev0303@gmail.com>
2023-01-27 11:10:17 +0300

    !!!!!!!!!!!
    !!!!!!!!!!!!!!!

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+// Instructions:
+// 1. Create a variable named "output" and assign it an empty array
+// 2. Create a for loop that starts at 1 and ends at 100
+// 3. Within the for loop, use an if/else statement to check if the current number is a multiple of both 3 and 5
+// 4. If the current number is a multiple of both 3 and 5, push "FizzBuzz" to the "output" array
+// 5. Else if the current number is a multiple of 3, push "Fizz" to the "output" array
+// 6. Else if the current number is a multiple of 5, push "Buzz" to the "output" array
+// 7. Else push the current number to the "output" array
+// 8. Create a function named "printOutput" that takes in no arguments and uses the "output" array to console.log each element
+// 9. Call the "printOutput" function
 
-let output = []
+// Note: You can use Ternary operator inside if/else statement for more concise and readable code.
 
-for (let empty = 1; empty <= 100; empty++){
-    console.log(`salom ${empty}`)
+let output = [];
+
+for (let empty = 1; empty <= 100; empty++) {
+  if (empty % 3 === 0 && empty % 5 === 0) {
+    output.push("Fizzbuzz");
+  } else if (empty % 3 === 0) {
+    output.push("Fizz");
+  } else if (empty % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(empty);
+  }
 }
+
+const printOutput = () => {
+  console.log(output);
+};
+
+printOutput();
